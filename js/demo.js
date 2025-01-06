184dead718962bf1cd485368c2a7547088eaf4a2
mwiinmhe <mwindjoummhoumadi78@hotmail.com>
2025-01-06 16:08:42 +0100

    css html js

---
+++ js/demo.js
@@ -0,0 +1,14 @@
+() => {};
+/* /notre code ici/ */
+let x = Number(10);
+let y = String("10");
+let total = x + y.length;
+const code = parseInt(3.5);
+
+const lang = ["fr", "it", "en"];
+
+console.table(lang.length);
+x === y ? console.log(true) : console.error(false);
+/*objet;methode;propriété: values*/
+console.log(code);
+console.log("Résultat" + total);
